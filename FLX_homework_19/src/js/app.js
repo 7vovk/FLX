73b5f30bf968f2b31a3f40ebecfe5c31eda01ca0
7vovk <7vovk@rambler.ru>
2019-04-06 09:07:06 +0300

    hw19: remove unnecessary variables

--- FLX_homework_19/src/js/app.js
+++ FLX_homework_19/src/js/app.js
@@ -2,18 +2,15 @@ let getId = (id) => {
     return document.getElementById(id);
 };
 let rock = 1, paper = 2, scissors = 3, index = 1, count = 0;
-let p1 = document.createElement('p'), p2 = document.createElement('p'), p3 = document.createElement('p');
+let p1 = document.createElement('p'), p2 = document.createElement('p');
 
 let theGame = (userMove) => {
     if (index < 3) {
-        let p1 = document.createElement('p'), p2 = document.createElement('p'), p3 = document.createElement('p');
         let p = 'p' + index;
         if (index === 1) {
             getId('gameInfo').appendChild(p1); p1.setAttribute('id', 'p1');
-        } else if (index === 2) {
-            getId('gameInfo').appendChild(p2); p2.setAttribute('id', 'p2');
         } else {
-            getId('gameInfo').appendChild(p3); p3.setAttribute('id', 'p3');
+            getId('gameInfo').appendChild(p2); p2.setAttribute('id', 'p2');
         }
         let move = Math.floor(Math.random() * 3) + 1;
         if (userMove === rock) {
